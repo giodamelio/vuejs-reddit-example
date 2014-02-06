afe549941d36f96b4708007fe0ce5e8648a62237
Gio d'Amelio <giodamelio@gmail.com>
2014-02-05 22:03:38 -0800

    Show basic reddit data from /r/all

--- js/main.js
+++ js/main.js
@@ -1,13 +1,43 @@
-var subredditSelector = new Vue({
-    el: "#subredditSelector",
+var app = new Vue({
+    el: "#app",
     data: {
-        activeSubreddit: "all"
+        activeSubreddit: "all",
+        subredditsList: ["all", "askreddit", "starbound"],
+        subreddits: {}
     },
     methods: {
         selectSubreddit: function(e) {
             // Select our subreddit
             this.activeSubreddit = e.el.innerText.toLowerCase()
-            console.log("Enabling subreddit", e.el.innerText.toLowerCase())
+            app.$emit("subreddit switch", e.el.innerText.toLowerCase());
         }
     }
 })
+
+// Get data from subreddit
+getSubredditData = function(subreddit, callback) {
+    reqwest({
+        url: "http://api.reddit.com/r/" + subreddit + ".json", 
+        method: "GET",
+        type: "jsonp",
+        jsonpCallback: "jsonp",
+        success: function(res) {
+            callback(res.data);
+            app.$emit("subreddit download", subreddit);
+        }
+    });
+}
+
+// Get subreddit data on startup
+getSubredditData("all", function(data) {
+    data.children = data.children.map(function(item) {
+        console.log(item.data)
+        return item.data;
+    });
+    app.subreddits.all = data.children;
+});
+
+// Log some stuff
+app.$on("subreddit switch", function(subreddit) {
+    console.log("Switching to subreddit /r/" + subreddit);
+});
